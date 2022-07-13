c71fa83f475157b8810d3598ec20b34290aebd24
rainflore <egyramadhan3@gmail.com>
2022-07-13 14:35:09 +0700

    Update CoinItem.jsx

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const CoinItem = (props) => {
   return (
     <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
-      <div className="">
+      <div className="w-full p-10">
         <img src={props.coin.image} alt="crypto" className="w-[50px]" />
-        <h1 className="">{props.coin.name}</h1>
-        <p className="">{props.coin.symbol}</p>
+        <h1 className="uppercase mt-5">{props.coin.name}</h1>
+        <p className="font-bold">{props.coin.symbol}</p>
         <p className="">$ {props.coin.current_price.toLocaleString()}</p>
         <p className="">$ {props.coin.total_volume.toLocaleString()}</p>
       </div>
